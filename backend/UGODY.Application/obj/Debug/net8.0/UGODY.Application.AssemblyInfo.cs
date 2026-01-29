8ee736797c49b7533fc49c235bdd9225583a3ddf
Aithentica <bohdan.glowacki@aithentica.pl>
2026-01-29 13:24:42 +0100

    f

--- backend/UGODY.Application/obj/Debug/net8.0/UGODY.Application.AssemblyInfo.cs
+++ backend/UGODY.Application/obj/Debug/net8.0/UGODY.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UGODY.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf4054673a48a1e71eb876552f8fe6bd50dca6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95e68614f15f32901abdc5760d60667963726511")]
 [assembly: System.Reflection.AssemblyProductAttribute("UGODY.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UGODY.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
